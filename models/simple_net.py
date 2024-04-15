779226c9941b51334e1190b706d44afe6dbf0f73
brkn8102 <111605372+bknutson0@users.noreply.github.com>
2024-04-15 17:00:24 -0600

    select optimizer and loss criterion

--- models/simple_net.py
+++ models/simple_net.py
@@ -8,8 +8,17 @@ class SimpleNet(torch.nn.Module):
         self.fc1 = nn.Linear(input_dim, output_dim)
         self.fc2 = nn.Linear(output_dim, output_dim)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
+    def forward(self, X):
+        X = self.fc1(X)
+        X = self.fc2(X)
+        return X
+    
+    def train_step(self, X, Y):
+        self.optimizer.zero_grad() 
+        output = self(X)
+        loss = self.criterion(output, Y)
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+    
     
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ input_dim = 10
 output_dim = 20
 dataset_size = 1000
 model = models.simple_net.SimpleNet(input_dim, output_dim)
-epochs = 100
+max_epochs = 10
 batch_size = 10
 
 # Synthesize data 
@@ -17,6 +17,8 @@ data_utils.synthesize_data(models.simple_net.SimpleNet, input_dim, output_dim, d
 
 # Instantiate the model
 net = models.simple_net.SimpleNet(input_dim, output_dim)
+net.optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+net.criterion = torch.nn.MSELoss()
 
 # Load the data
 dataset_dict = torch.load('data/dataset.pth')
@@ -25,16 +27,17 @@ Y = dataset_dict['Y']
 
 # Train the model using batched SGD and save losses 
 # TODO: create option for randomly sampled batches instead of epoch-wise
+epochs = []
 losses = []
-for epoch in range(epochs):
+for epoch in range(max_epochs):
     for i in range(0, dataset_size, batch_size):
         X_batch = X[i:i+batch_size]
         Y_batch = Y[i:i+batch_size]
         loss = net.train_step(X_batch, Y_batch)
+        epochs.append(epoch + i/dataset_size)
         losses.append(loss)
-
 # Plot loss versus epoch
-plt.plot(losses)
+plt.plot(epochs, losses)
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title('Training Loss')
