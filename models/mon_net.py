4c804cf96ddb82c74334df210b37faddbd4b5829
brkn8102 <111605372+bknutson0@users.noreply.github.com>
2024-05-09 12:47:01 -0600

    add comments and relu

--- models/mon_net.py
+++ models/mon_net.py
@@ -5,6 +5,11 @@ import torch.nn.functional as F
 # Based on https://github.com/locuslab/monotone_op_net/blob/master/mon.py
 
 class MonLayer(torch.nn.Module):
+    """ Single monotone layer that computes
+            z_(k+1) = ReLU(W z_k + U x + b)
+        where 
+            W = (1-m)I - A^T A + B - B^T.
+    """
     def __init__(self, in_dim, out_dim, m=1.0):
         super().__init__()
         self.U = nn.Linear(in_dim, out_dim)
@@ -16,10 +21,19 @@ class MonLayer(torch.nn.Module):
         return 'MonNet'
 
     def forward(self, x, z):
-        return self.multiply(z) + self.U(x)
+        return F.relu(self.multiply(z) + self.U(x))
 
     def multiply(self, z):
         ATAz = self.A(z) @ self.A.weight 
         return (1 - self.m) * z - ATAz + self.B(z) - z @ self.B.weight
     
+class MonNet(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, m=1.0):
+        super().__init__()
+        self.layer = MonLayer(in_dim, out_dim, m)
     
+    def name(self):
+        return 'MonNet'
+
+    def forward(self, x, z):
+        return self.layer(x, z)
