1ac8eb789c19a6a860f4b2826cd6931971d233ac
Brandon Knutson <bknutson@mines.edu>
2024-06-09 13:35:29 -0600

    fix seed test

--- unit_tests/test_seed.py
+++ unit_tests/test_seed.py
@@ -1,13 +1,13 @@
 import torch
-from models.mon_net_AD import MonNetAD
+from models.mon_net import MonNetAD
 from utils.model import set_seed
 
 set_seed(1)
-model_1 = MonNetAD(1, 1)
+model_1 = MonNetAD()
 for name, param in model_1.named_parameters():
     print(f'Parameter: {name}, Initial value: {param.detach().cpu().numpy()}')
 
 set_seed(2)
-model_2 = MonNetAD(1, 1)
+model_2 = MonNetAD()
 for name, param in model_2.named_parameters():
     print(f'Parameter: {name}, Initial value: {param.detach().cpu().numpy()}')
